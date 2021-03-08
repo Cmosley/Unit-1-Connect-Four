17fd179ae227d84297628a6c98d941e98fa99f84
cmosley <cmosley@beholdmedia.com>
2021-03-07 20:56:35 -0600

    Alter approach for game piece logic

--- js/app.js
+++ js/app.js
@@ -2,101 +2,77 @@
 
 PLAYER_TURN = {
     'null': '',
-    '1' : 'red',
-    '-1' : 'yellow'
+    '1' : 'red-piece',
+    '-1' : 'yellow-piece'
 }
 
-
-// array for each row game of cells, 
-// will link the hover feature using bracket notation
-const boardCols = [
-    [0,7,14,21,28,35],
-    [1,8,15,22,29,36],
-    [2,9,16,23,30,37],
-    [3,10,17,24,31,38],
-    [4,11,18,25,32,39],
-    [5,12,19,26,33,40],
-    [6,13,20,27,34,41]
-]
-
- 
-
-const winningCombos = [
-    // either hard code each winning combo (inefficient) 
-    // figure out how to write function to check neighbors, 
-    // stop at 4 in a row (more efficient)
-]
 
 /*----- app's state (variables) -----*/ 
 let grid, winner, turn
 
 /*----- cached element references -----*/ 
 
-// Might need to add row ontop of grid and use that for pieceDrop
-// functionality instead of each element in grid being selectable
-const whichCol = document.querySelectorAll('#column-drop > div')
-const grabcol = document.querySelectorAll('.grid > div')
+// const whichCol = document.querySelectorAll('#column-drop > div')
+const gridBoxes = document.querySelectorAll('.grid > div')
 const resetBtn = document.getElementById('reset')
 const howToPlay = document.getElementById('how-to')
 const winMsg = document.getElementById('winMsg')
 
 /*----- event listeners -----*/ 
 
-whichCol.forEach(circle => {
-    circle.addEventListener('click', pieceDrop)
+// whichCol.forEach(circle => {
+//     circle.addEventListener('click', pieceDrop)
+// });
+
+gridBoxes.forEach(circle => {
+    circle.addEventListener('click', setPiece)
 });
 
 
 
 /*----- functions -----*/
 
+function setPiece () {
 
-function pieceDrop (e) {
-    // gets index of which column to drop piece 
-    let colNum = (parseInt(e.target.id))
-    // iterate column to check for first null value to drop piece
-
-    // we get the index of where the piece is being dropped
-    index = grid[colNum].length
-    // while(index-- && grid[colNum][index])
-    while(index-- && (grid[colNum][index] === null))
-    grid[colNum][index] = turn
-    console.log(turn);
-    console.log(grid[colNum][index]);
-    // console.log(grid[sqIdx].length - grid[sqIdx].slice(0).reverse().findIndex(function (el) { return el }) - 1);
 }
-    // if(boardCols[sqIdx] === null){
 
-    // }
+// function pieceDrop (e) {
+//     // gets index of which column to drop piece 
+//     let colNum = (parseInt(e.target.id))
+//     let columnSelect = grid[colNum]
+//     // iterate column to check for first null value to drop piece
+//     // we get the index of where the piece is being dropped
+//     index = grid[colNum].length
+//     // while(index-- && grid[colNum][index])
+//     while(index-- && !grid[colNum][index]) 
+//         if (grid[colNum][index] === null)
+//     // grid[colNum][index] = turn
+//     grid[colNum].splice((index - 1),1,turn)
+//     console.log(turn);
+//     console.log(grid[colNum]);
+   
+//     render();
+// }
+//     // we use that index to search the nested array for where the piece 
+//     // should go. We then need to "drop" piece to the farthest empty cell
     
-    
-    // console.log(boardCols[0])
-    // let colDrop = boardCols[sqIdx]
-    // }
-    // for (i in sqIdx)
-    // if(boardCols[i] === null)
-    // boardCols[i].push(sqIdx)
+//     //  change players
+//     // turn *= -1,
 
-    
-    // we use that index to search the nested array for where the piece 
-    // should go. We then need to "drop" piece to the farthest empty cell
-    // for (x=0; x < 7; x++){
-    
-    // boardColumns[`${sqIdx}`]
-    //  change players
-    // turn *= -1,
 
 function render () {
+    // update game cell with current players game piece color
     
 }
 
-function ckWin () {
+function checkWin () {
     // check columns for 4 in a row
 
     //  check rows for 4 in a row
 
     // check diagnoally for 4 in a row
 }
+
 // grid functionality 
 
 //  1. Each row is an array, with the bottom of the row being the last element in array. Each array would need to inialized as empty, then use index of 
@@ -107,7 +83,7 @@ function ckWin () {
 function init () {
     turn = 1; 
     grid = [
-        [null,null,null,null,"1","-1"],
+        [null,null,null,null,"1","2"],
         [null,null,null,null,null,"1"],
         [null,null,null,null,null,null],
         [null,null,null,null,null,null],
@@ -115,17 +91,8 @@ function init () {
         [null,null,null,null,null,null],
         [null,null,null,null,null,null],
     ];
-
-
+    winner = false;
+    render();
 }
 
 init()
-
-
-// var array = [-1, null, null, 1, null],
-//     index = array.length;
-    
-// while (index-- && array[index]);
-
-// console.log(index);
-// console.log(array[index]);
