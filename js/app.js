c1246f23ebe8a974ac1c283361f491482296747e
cmosley <cmosley@beholdmedia.com>
2021-03-09 15:25:47 -0600

    column and row win logic

--- js/app.js
+++ js/app.js
@@ -46,7 +46,7 @@ function gameDivID () {
     gridBoxes.forEach(cells => {
         cells.setAttribute('id', count)
         count++;
-        console.log(cells)
+        // console.log(cells)
     })
 }
 
@@ -55,43 +55,78 @@ function gameDivID () {
 function setPiece (e) {
     // gets index of cell to 'drop' piece into
     let divID = (parseInt(e.target.id))
+    // console.log(divID)
     if (grid[divID] === null){
         const clickedDiv = document.getElementById(divID)
         clickedDiv.classList.add(`${PLAYER_TURN[turn]}`)
-    
-    
-    console.log(divID)
-
-    turn *= -1
-    winner = checkWin()
+            if(checkWin(PLAYER_TURN[turn])){
+                alert(`${PLAYER_TURN[turn] + ' has won!'}`);
+                winner = PLAYER_TURN[turn]
+            }
+            turn *= -1
+    // winner = checkWin(PLAYER_TURN[turn])
     render();    
     }
 }
-    
-    
-    // iterate column to check for first null value to drop piece
-    // we get the index of where the piece is being dropped
 
-    // we use that index to search the nested array for where the piece 
-    // should go. We then need to "drop" piece to the farthest empty cell
-    
- 
-  // update new values here, not add new value
 function render () {
     // update game cell with current players game piece color
     gridBoxes.forEach((cell, idx) => {
-        console.log()
+        // console.log(cell)
     })
 }
 
 // PLAYER_TURN[grid[idx]]
-function checkWin () {
+function checkWin (player) {
     // check columns for 4 in a row
-    
+    let combo = 0;
+    for(let i =0; i < 42; i+=7){
+        for(let k=0; k < 7; k++){
+            let cell = gridBoxes[`${i+k}`];
+            // console.log(cell)
+            if(cell.className === `${PLAYER_TURN[turn]}`){
+                combo++
+            } else {
+                combo = 0;
+            }
+            console.log(`columns: ${combo}`)
+            if(combo >= 4) {
+                return true;
+            }
+            
+        }
+        combo = 0;
+    }
     //  check rows for 4 in a row
+    combo = 0;
+    for(let i =0; i < 7; i++){
+        for(let k=0; k < 42; k+=7){
+            let cell = gridBoxes[`${i+k}`];
+            // console.log(cell)
+            if(cell.className === `${PLAYER_TURN[turn]}`){
+                combo++
+            } else {
+                combo = 0;
+            }
+            console.log(`rows + ${combo}`)
+            if(combo >= 4) {
+                return true;
+            }
+        }
+        combo = 0;
+    }
+    // check diag
+
+
+    return false;
+}        
+    
+    
+
+    
 
     // check diagnoally for 4 in a row
-}
+
 
 // grid functionality 
 
